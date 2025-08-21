573f160e1c42d5baa70c208da9846dcf2d1ba99a
JAP994 <sucop@MacBook-Pro-de-Jonathan.local>
2025-08-21 15:33:51 -0500

    Checklist ui controls finalizado

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -7,6 +7,95 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('UI Controls')),
+      body: const _UiControlsView(),
+    );
+  }
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+  bool wantsBreackfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: isDeveloper,
+          onChanged: (value) => setState(() {
+            isDeveloper = value;
+          }),
+        ),
+
+        ExpansionTile(
+          title: const Text('Vehiculo de transporte'),
+          subtitle: Text('$selectedTransportation'),
+          children: [
+            RadioGroup<Transportation>(
+              groupValue: selectedTransportation,
+              onChanged: (Transportation? value) {
+                if (value != null) {
+                  setState(() {
+                    selectedTransportation = value;
+                  });
+                }
+              },
+              child: Column(
+                children: Transportation.values.map((transport) {
+                  return RadioListTile<Transportation>(
+                    value: transport,
+                    title: Text(transport.name),
+                    subtitle: Text('Descripción de ${transport.name}'),
+                    activeColor: Colors.blue,
+                    controlAffinity: ListTileControlAffinity.trailing,
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+
+        CheckboxListTile(
+          title: const Text('Desayuno?'),
+          value: wantsBreackfast,
+          onChanged: (value) => setState(() {
+            wantsBreackfast = !wantsBreackfast;
+          }),
+        ), 
+        CheckboxListTile(
+          title: const Text('Almuerzo?'),
+          value: wantsLunch,
+          onChanged: (value) => setState(() {
+            wantsLunch = !wantsLunch;
+          }),
+        ), 
+        CheckboxListTile(
+          title: const Text('Cena?'),
+          value: wantsDinner,
+          onChanged: (value) => setState(() {
+            wantsDinner = !wantsDinner;
+          }),
+        ), 
+        
+        // Grupo de botones de opción con título y subtítulo personalizados
+      ],
+    );
   }
 }
