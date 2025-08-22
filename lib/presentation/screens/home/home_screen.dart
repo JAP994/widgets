1e14408743a54caa89d18a37fcb7a626207a1fd7
JAP994 <sucop@MacBook-Pro-de-Jonathan.local>
2025-08-22 13:13:38 -0500

    Implementacion de side menu integrar funcion para cerrar slide automaticamente

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -9,10 +9,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(title: Text('Flutter + Material 3'), centerTitle: false),
       body: _HomeView(),
-      drawer: SideMenu(),
+      drawer: SideMenu( scaffoldKey: scaffoldKey,),
     );
   }
 }
